dcf9c8f11ae6aa037db7398f07044aa3aac329af
Taf009 <tafmilazi@gmail.com>
2026-01-05 00:28:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -482,8 +482,6 @@ with tab2:
 
             submitted = st.form_submit_button("Complete Order & Send PDF")
 
-            submitted = st.form_submit_button("Complete Order & Send PDF")
-
             if submitted:
                 if not client_name or not order_number or not operator_name:
                     st.error("Client Name, Order Number, and Your Name are required")
